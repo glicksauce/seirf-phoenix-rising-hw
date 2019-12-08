71c91fb500de59fbdd6f8bebbbcfb0cab2d24fce
Jay Glickman <jayglickman@gmail.com>
2019-12-08 17:57:13 -0500

    finished loop practice

---
+++ w01_d3/loops.js
@@ -0,0 +1,26 @@
+var i = 0;
+//Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (i=0; i<100; i=i+2){
+    console.log(i);
+}
+console.log(" ");
+//Create a loop that logs the ODD numbers from 99-0 (descending)
+for (i=99; i>0; i=i-2){
+    console.log(i);
+}
+console.log(" ");
+//Create a loop that logs the numbers from 49-72 (ascending)
+for (i=49; i<=72; i++){
+    console.log(i);
+}
+console.log(" ");
+//Create a loop that logs the numbers from 81-26 (descending)
+for (i=81; i>=26; i--){
+    console.log(i);
+}
+console.log(" ");
+//Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (i=3; i<=90; i=i+3){
+    console.log(i);
+}
+console.log(" ");
