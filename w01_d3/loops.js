151c3835845c5e925889c4cc4888001575b4f014
Jay Glickman <jayglickman@gmail.com>
2019-12-08 20:43:17 -0500

    current week homework

--- w01_d3/loops.js
+++ w01_d3/loops.js
@@ -1,26 +1,53 @@
+
 var i = 0;
-//Create a loop that logs the EVEN numbers from 0-98 (ascending)
+//1) Create a loop that logs the numbers from 0-99 (ascending)
+for (i=0; i<=99; i++){
+    console.log(i);
+}
+console.log(" ");
+
+//2) Create a loop that logs the numbers from 99-0 (descending)
+for (i=99; i>=0; i--){
+    console.log(i);
+}
+console.log(" ");
+
+//3) Create a loop that logs the EVEN numbers from 0-98 (ascending)
 for (i=0; i<100; i=i+2){
     console.log(i);
 }
 console.log(" ");
-//Create a loop that logs the ODD numbers from 99-0 (descending)
+//4) Create a loop that logs the ODD numbers from 99-0 (descending)
 for (i=99; i>0; i=i-2){
     console.log(i);
 }
 console.log(" ");
-//Create a loop that logs the numbers from 49-72 (ascending)
+
+//5) Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (i=1; i<=99; i=i+2){
+    console.log(i);
+}
+console.log(" ");
+
+//6) Create a loop that logs the ODD numbers from 99-0 (descending)
+for (i=99; i>=0; i=i-2){
+    console.log(i);
+}
+console.log(" ");
+
+//7) Create a loop that logs the numbers from 49-72 (ascending)
 for (i=49; i<=72; i++){
     console.log(i);
 }
 console.log(" ");
-//Create a loop that logs the numbers from 81-26 (descending)
+//8) Create a loop that logs the numbers from 81-26 (descending)
 for (i=81; i>=26; i--){
     console.log(i);
 }
 console.log(" ");
-//Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+//9) Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
 for (i=3; i<=90; i=i+3){
     console.log(i);
 }
 console.log(" ");
+
