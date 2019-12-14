4a341a3d4dbb42a528918618eb78241337e5db18
Jay Glickman <jayglickman@gmail.com>
2019-12-14 17:29:24 -0500

    afternoon_lab_solutions complete

--- w02_d2/afternoon_lab_solutions.js
+++ w02_d2/afternoon_lab_solutions.js
@@ -244,6 +244,19 @@ array3 = [[1,2,3,4],
          [12,19,18,7],
          [11,10,9,8]];
 
+         c = [
+          [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
+          [36, 37, 38, 39, 40, 41, 42, 43, 44, 11],
+          [35, 64, 65, 66, 67, 68, 69, 70, 45, 12],
+          [34, 63, 84, 85, 86, 87, 88, 71, 46, 13],
+          [33, 62, 83, 96, 97, 98, 89, 72, 47, 14],
+          [32, 61, 82, 95, 100, 99, 90, 73, 48, 15],
+          [31, 60, 81, 94, 93, 92, 91, 74, 49, 16],
+          [30, 59, 80, 79, 78, 77, 76, 75, 50, 17],
+          [29, 58, 57, 56, 55 ,54, 53, 52, 51, 18],
+          [28, 27, 26, 25, 24, 23, 22, 21, 20, 19]
+       ];
+
 /*
 then
 
@@ -317,5 +330,21 @@ function spiral (arr){
     
     return (finalArr);
 }
+console.log(c)
+console.log(spiral(c));
 
-console.log(spiral(array3));
+//FizzBuzz. if mulitiple of 3 print fizz if muliple of 5 print buzz if both print fizzbuzzx
+for (i=1;i<=100;i++){
+  word = [i];
+  output = '';
+  if (i%3 == 0){
+    word.push("fizz");
+  }
+  if (i%5 == 0){
+    word.push("buzz")
+  }
+  output = word.toString();
+  console.log(output);
+  word = '';
+  output = '';
+}
