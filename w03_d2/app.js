2fa6b706a2db322af279f063368b7b4521888010
Jay Glickman <jayglickman@gmail.com>
2019-12-22 17:11:53 -0500

    started CSS styling, added store feature and checkout feature

--- w03_d2/app.js
+++ w03_d2/app.js
@@ -12,13 +12,33 @@ console.log("up and running");
     //when when x lawns are mowed. Maybe do a grid of neighborhoods. Stretch goal lawns can regrow and you have to mow faster than they regrow
 //Hint: Try setting the win amount to $10 frst, then $50, and etc. until you get to the end to allow for easier testing!
 
+//Variable and Object delcarations
+let day = 1;
 
+
+const tool = {
+  teeth: {cost: 1, daysEarnings: 1, formattedName: 'Teeth'},
+  rustyScissors: {cost: 5, daysEarnings: 5, formattedName: 'Rusty Scissors'},
+  pushMower: {cost: 25, daysEarnings: 50, formattedName: 'Old-Timey Push Mower'},
+  batteryMower: {cost: 250, daysEarnings: 250, formattedName: "Battery-Powered Lawnmower"},
+  starvingStudents: {cost: 500, daysEarnings: 250, formattedName: "Team of Starving Students"}
+}
+
+const player = {
+  name: '',
+  tool: [tool.teeth],
+  wallet: 0
+}
+
+const town = {};
+
+let name = prompt("Please enter your name", "enter name");
 
 //Function Delcarations
   //When user clicks 'Lets Cut Some Grass' Button
 function doWork(){
-  let dayTool = player.tool;
-  let dayEarnings = tool[dayTool].daysEarnings
+  let dayTool = player.tool[0].formattedName;
+  let dayEarnings = player.tool[0].daysEarnings
   walletAdjust(dayEarnings);
   let dayMessage = "You went to work today!<br>You mowed with your " + dayTool + "<br> and you earned $" + dayEarnings + " today!";
   document.getElementById("dayStart").innerHTML = dayMessage;
@@ -45,39 +65,37 @@ function hideButtons(){
 
 function goShopping(){
   let toolList = '';
+
   document.getElementById("nextMove").innerHTML = "You have $" + player.wallet + " in your wallet. What would you like to buy?"
 
+  //let buttonProperties = document.getElementsByClassName('buyButton');
+  //buttonProperties.display = "block";
+  //buttonProperties.width = "100px";
+
   for (let toolName in tool){
-    toolList += tool[toolName].formattedName + " $" + tool[toolName].cost + "<br>";
-    //toolList += toolName;
+    let buttonName = toolName + "button";
+    document.getElementById(toolName).innerHTML = "<button class='buyButton' id=" + buttonName + ">buy</button>" + tool[toolName].formattedName + " $" + tool[toolName].cost + "<br>";
+    document.getElementById(buttonName).addEventListener ("click", function() {
+      attemptBuy(tool[toolName].cost, tool[toolName].formattedName);
+    });
   }
-  document.getElementById("storeGrid").innerHTML = toolList;
+
+  
 }
 
 function walletAdjust(dollars) {
   player.wallet += dollars;
 }
 
-//Variable and Object delcarations
-let day = 1;
-
-const player = {
-    name: '',
-    tool: 'teeth',
-    wallet: 0
+function attemptBuy(itemCost, toolFormattedName){
+  if (itemCost <= player.wallet){
+    alert("congrats you bought " + toolFormattedName + "!");
+    player.wallet -= itemCost;
+  } else {
+    alert("sorry you can't afford that");
+  }
+  document.getElementById("nextMove").innerHTML = "You have $" + player.wallet + " in your wallet";
 }
-
-const tool = {
-  teeth: {cost: 0, daysEarnings: 1, formattedName: 'Teeth'},
-  rustyScissors: {cost: 5, daysEarnings: 5, formattedName: 'Rusty Scissors'},
-  pushMower: {cost: 25, daysEarnings: 50, formattedName: 'Old-Timey Push Mower'},
-  batteryMower: {cost: 250, daysEarnings: 250, formattedName: "Battery-Powered Lawnmower"},
-  starvingStudents: {cost: 500, daysEarnings: 250, formattedName: "Team of Starving Students"}
-}
-
-const town = {};
-
-let name = prompt("Please enter your name", "enter name");
 
 //Start of Game logic
   //prompts for name and does welcome message
@@ -87,7 +105,7 @@ if (name != null) {
 
     player.name =name;
 
-    document.getElementById("dayStart").innerHTML = "Its day " + day + ". You have $" + player.wallet + " in your wallet. You have " + player.tool + " in your toolbelt. What would you like to do today?";
+    document.getElementById("dayStart").innerHTML = "Its day " + day + ". You have $" + player.wallet + " in your wallet. You have " + player.tool[0].formattedName + " in your toolbelt. What would you like to do today?";
 }
 
 //Listeners for 3 buttons
