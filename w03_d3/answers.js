e0faefef2e649fd7ee9430b21478db6053e02c84
Jay Glickman <jayglickman@gmail.com>
2020-01-01 12:40:10 -0500

    objectception complete

--- w03_d3/answers.js
+++ w03_d3/answers.js
@@ -122,3 +122,26 @@ console.log(crazyObject.larry.characters[1]);
 crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
 console.log(crazyObject.larry.quotes[4]);
 
+//----------------------------------------
+//Object-ception
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+ //Change the value of limbo to null.
+ inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+ console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6);
