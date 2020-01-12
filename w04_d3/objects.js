66a48dd4032b92bebc4487030caba4b788ccbbd9
Jay Glickman <jayglickman@gmail.com>
2020-01-12 10:25:08 -0500

    created hamster class

--- w04_d3/objects.js
+++ w04_d3/objects.js
@@ -31,6 +31,53 @@ class Hamster {
     }
 }
 
-const me = new Hamster();
+class Person {
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
 
-console.log(me.getPrice());
+    getName (){
+        return this.name;
+    }
+
+    getAge() {
+        return this.age
+    }
+
+    getWeight(){
+        return this.weight
+    }
+
+    greet(){
+        console.log("hello " + this.name)
+    }
+
+    eat(){
+        this.weight += .5 * this.mood
+    }
+
+    exercise(){
+        this.weight -= 1;
+    }
+
+    ageUp(){
+        this.age++;
+        this.height++
+        this.weight += 2;
+        this.mood --;
+        this.bankAccount += 10
+    }
+
+    buyHamster(hamster){
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        let hamsterPrice = Hamster.getPrice(hamster);
+        this.bankAccount -= hamsterPrice;
+    }
+}
