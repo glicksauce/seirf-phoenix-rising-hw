52db47d5e92ddaf45b013f07bf74458ee89a3a22
Jay Glickman <jayglickman@gmail.com>
2020-01-12 11:36:47 -0500

    dougie and pizza rat have fought

--- w04_d3/donut_adventure/adventure.js
+++ w04_d3/donut_adventure/adventure.js
@@ -21,8 +21,15 @@ class Hero{
         console.log(this.health);
     }
 
-    fight(){
-        console.log('i\'m ready to rumble');
+    fight(Enemy){
+        let keys = Object.keys(this.weapons)
+        let randomWeapon = Math.floor(Math.random() * keys.length);
+        let keyName = keys[randomWeapon]
+        console.log('i\'m ready to rumble with my ' + keyName + " it deals " + this.weapons[keyName] + " damage!")
+
+        Enemy.health -= this.weapons[keyName]
+
+        console.log("Take that! " + Enemy.name + "'s health is now " + Enemy.health)
     }
 }
 
@@ -51,11 +58,49 @@ class Enemy{
         console.log(this.health);
     }
 
-    fight(){
-        console.log('i\'m gonna flatten you like a slice of pepperoni!');
+    fight(Enemy){
+        let keys = Object.keys(this.weapons)
+        let randomWeapon = Math.floor(Math.random() * keys.length);
+        let keyName = keys[randomWeapon]
+        console.log('i\'m gonna flatten you like a slice of pepperoni! with my ' + keyName + ". It deals " + this.weapons[keyName] + " damage!");
+
+        Enemy.health -= this.weapons[keyName];
+
+        console.log("Take that! " + Enemy.name + "'s health is now " + Enemy.health);
     }
 }
 
 const Ronnie = new Enemy('Ronnie')
 
 console.log(Ronnie);
+
+/*
+Walking Down the Street
+Now, let's write their story! Dougie is walking down Flat Iron -- but oh no! He runs into Pizza rat!
+
+Have Dougie talkSass
+Have Pizza Rat talkSmack
+Have Dougie announceHealth
+Have Pizza Rat announceHealth
+*/
+
+Dougie.talkSass();
+Ronnie.talkSmack();
+Dougie.announceHealth();
+Ronnie.announceHealth();
+
+/*
+Fight!
+Things have escalated between Dougie and Pizza Rat!
+
+Upgrade their fight methods so that it accesses one of their weapons and console log its hitpoints.
+Keep upgrading this fight method to accept an argument called enemy so that when you call on the fight method you can pass in the entire Dougie or Pizza Rat object as the parameter. (i.e. dougie.fight(pizzaRat))
+Now that we are able to pass in Dougie or Pizza Rat as an object, we can make it so that our fight method subtracts from their health.
+Using the hitpoints from the weapon they're using, subtract that amount from the enemy's health (i.e. If Dougie fights Pizza Rat using sprinkleSpray, subtract sprinkleSpray's hitpoint value (5) from Pizza Rat's health)
+Console log the enemy name and their new health value (i.e. 'Dougie got hit by pepperoniStars! His health is now at 95!')
+*/
+
+Dougie.fight(Ronnie);
+Ronnie.fight(Dougie);
+Dougie.announceHealth();
+Ronnie.announceHealth();
