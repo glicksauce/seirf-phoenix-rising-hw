53560e2a1f17d7d9f1e1bb5b07242b60e088ce10
Jay Glickman <jayglickman@gmail.com>
2020-01-17 13:52:38 -0500

    Commit 2 - I added more elements, My power grows with my dedication and hard work

--- w05_d3/hogwarts/app.js
+++ w05_d3/hogwarts/app.js
@@ -5,18 +5,23 @@ $(() => {
         console.log('oops! I still have to link my jQuery properly!');
     } else {console.log('I did it! I linked jQuery and this js file!')};
 
+    //Year 1
     const $container =  $('#container')
-
-    console.log($container)
-
     const $h1 = $("<h1>").text("Hogwarts");
     
     $($container).append($h1);
 
-    console.log($h1)
 
+    //Year 2
+    const $h2 = $("<h2>").text("Jay G")
+    const $h3 = $("<h3>").text("House: Hufflepuff")
+    const $pet = $("<h4>").addClass("quokka").text("Pet: Henry")
+    const $wand = $("<h4>").text("Wand: Hornbeam Wand with Dragon Heartstring Core")
 
-
+    $($container).append($h2)
+    $($container).append($h3)
+    $($container).append($pet)
+    $($container).append($wand)
 
 
 });
