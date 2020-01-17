c1b8bdbecb1617a4e623b29eacb4f93e74248a92
Jay Glickman <jayglickman@gmail.com>
2020-01-17 14:43:18 -0500

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- w05_d3/hogwarts/app.js
+++ w05_d3/hogwarts/app.js
@@ -39,5 +39,41 @@ $(() => {
         $($list).append(eval(strToAppend));
     }
     
+    //Year 4
+    const $table = $("<table>")
+    const $season = $("<h5>").text("Spring 2020")
+    const $thead = $("<thead>")
+    const $th1 = $("<th>").text("day")
+    const $th2 = $("<th>").text("Classes")
+    let $row = $("<tr>")
+    const $day1 = $("<td>").text("Monday")
+    const $day2 = $("<td>").text("Tuesday")
+    const $day3 = $("<td>").text("Wednesday")
+    const $day4 = $("<td>").text("Thursday")
+    const $day5 = $("<td>").text("Friday")
+    const $day1Schedule = $("<td>").text("Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice")
+    const $day2Schedule = $("<td>").text("Muggle Arts, Map Reading, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark ARts, Quidditch practice")
+    const $day3Schedule = $("<td>").text("Herbology, Vocabulary, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark ARts, Quidditch practice")
+    const $day4Schedule = $("<td>").text("Muggle Arts, Public Speaking, History of Magic, STudy Time, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice")
+    const $day5Schedule = $("<td>").text("Herbology, Divination, History of Magic, Charms, Poetry, Study Time, Public Speaking, Quidditch practice")
+
+
+    $($container).append($table);
+    $($table).before($season);
+    $($table).append($thead);
+    $($thead).append($th1);
+    $($thead).append($th2);
+    for (i=1;i<=5;i++){
+        let $row = $("<tr>")
+        let td1ToAdd = ('$day' + i)
+        let td2ToAdd = ('$day' + i + 'Schedule')
+        $($table).append($row);
+        $($row).append(eval(td1ToAdd));
+        $($row).append(eval(td2ToAdd));
+    }
+
+
+
+
 
 });
