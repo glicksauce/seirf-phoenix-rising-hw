e84698e206e5e44485f11564def144acd4304649
Jay Glickman <jayglickman@gmail.com>
2020-01-18 00:55:32 -0500

    more updates

--- w05_d3/hogwarts/app.js
+++ w05_d3/hogwarts/app.js
@@ -5,6 +5,38 @@ $(() => {
         console.log('oops! I still have to link my jQuery properly!');
     } else {console.log('I did it! I linked jQuery and this js file!')};
 
+    //functions:
+   
+    //brings in new wand:
+    const bringBackWand = () => {
+        $newWand = $("<h4>").text("Wand: Unicorn Tusk with Dragon's Tooth core")
+        $newWand.css("color","indigo")
+        $newWand.css("display","none")
+        setTimeout(function(){
+            $("h4").eq(0).after($newWand)
+            $newWand.fadeIn(2000);
+        },4000)
+    }
+
+    //bring back the Pet to original spot
+    const bringPetBack = () => {
+        $('h4').eq(0).after($petMission).css("display","hidden");
+        $petMission.fadeIn(1000);
+    }
+
+    //buy Butter Beer
+    const buyButterBeer = () =>{
+        $("ul").prepend($moreButterBeer).hide().fadeIn(1000);
+    }
+
+    //change season
+    const changeSeason = () =>{
+        $("h5").fadeOut(function(){
+            $(this).text("Fall 2018").fadeIn();
+            buyButterBeer();
+        });
+    }
+
     //Year 1
     const $container =  $('#container')
     const $h1 = $("<h1>").text("Hogwarts");
@@ -33,7 +65,6 @@ $(() => {
     const $listItems6 = $("<li>").text("Bertie Bott's Every Flavor [Jelly] Beans.")
 
     $($container).append($list);
-    //$($list).append($listItems1);
     for (i=1;i<=6;i++){
         let strToAppend = ('$listItems' + i)
         $($list).append(eval(strToAppend));
@@ -72,31 +103,125 @@ $(() => {
         $($row).append(eval(td2ToAdd));
     }
 
-
     //Year 5
-    $wand.remove()
-    $("li")[0].remove()
-    $newWand = $("<h4>").text("Wand: Unicorn Tusk with Dragon's Tooth core")
-    $newWand.css("color","indigo")
-    $("h4").after($newWand);
+    //declaring new wand here before function calls it
+    const newWand = () =>{
+        $newWand = $("<h4>").text("Wand: Unicorn Tusk with Dragon's Tooth core")
+        $newWand.css("color","indigo")
 
+        $("h4").eq(0).after($newWand);
+        $newWand.hide(0);
+        $newWand.show(2000);
+    }
+
+    //your wand breaks
+    $wand.delay(1000).slideUp(1000).delay(2000);
+    $wand.queue(function(){
+        //keeps new wand from forming before slot is removed
+        newWand();
+        $(this).dequeue()
+    })
+    
+
+    
+
+    //you drank all your butter beer
+    $("li").eq(0).delay(2000).fadeOut(1000)
+    $("li").eq(0).remove();
+
+    //you get a new Wand
+    //see above
+  
+
+    /*
+    setTimeout(function(){
+        $("h4").eq(0).after($newWand)
+        $newWand.fadeIn(2000);
+    },4000)
+    setTimeout(function(){
+        $("h4").eq(0).after($newWand)
+        $newWand.fadeIn(11000);
+    },7500)
+    */
+ 
     //pet spy mission
-    let $petMission = $("h4")[0]
-    $("td")[5].append($petMission)
+    const $petMission = $("h4").first()
+    const $petMoveTo =  $("td").eq(5)
+    
+  
+    
+    //pet table disappear function goes here before it is called
+    const petTableDisappear = () =>{
+        $petMission.fadeOut(1000);
+        $petMission.queue(function(){
+            bringPetBack()
+            $(this).dequeue();
+        })
+    }
 
-    //pet return mission
-    $("h3").after($petMission)
+    //pet table appear function goes here before it is called
+    const petTableAppear = () =>{
+        $petMission.fadeIn(1000).delay(1000);
+        $petMoveTo.append($petMission);
+        $petMission.queue(function(){
+            petTableDisappear();
+            $(this).dequeue();
+        });
 
+    }
+
+    //pet leaves original spot
+    $petMission.delay(6000).fadeOut(2000).delay(500)
+    $petMission.queue(function(){
+        petTableAppear();
+        $(this).dequeue();
+    });
+
+    //pet appears on table
+    //see above
+
+    //pet leaves table
+    //see above
+
+    //pet reappears in original spot
+    //see above
+    
     //Year 6
-    $("[class='secret']").hide('slow').delay(2000).show(3000)
-    $("ul li:nth-child(4)").addClass('cabbage')
-    $("ul li:nth-child(4)").removeClass('cabbage')
+    //hide and reveal secret items:
+    $("[class='secret']").delay(13000).hide('slow').delay(2000).show(3000)
 
+    //remove cabblage class function
+    const removeCabbage = () =>{
+        $("ul li:nth-child(5)").removeClass('cabbage');
+        $("ul li:nth-child(5)").queue(function (){
+            changeSeason();
+            $(this).dequeue();
+        })
+    }
+
+    //leash becomes cabbage
+    $("ul li:nth-child(5)").delay(20000)
+    $("ul li:nth-child(5)").queue(function(){
+        $(this).addClass('cabbage')
+        removeCabbage();
+
+        $(this).dequeue();
+    });
+
+    
     //Year 7
-    $("h5").text("Fall 2018")
-    const $moreButterBeer = $("<li>").text("Butter beer");
-    $("[storage='trunk']").prepend($moreButterBeer)
+    
+    //update class schedule to Fall 2018
+    //see above
+
+
+    //buying more butter beer
+    const $moreButterBeer = $("<li>").text("Butter beer")
+
+    //$("[storage='trunk']").prepend($moreButterBeer)
     $("[storage='trunk']").attr("storage","chest")
+    
+ 
 
 
 });
