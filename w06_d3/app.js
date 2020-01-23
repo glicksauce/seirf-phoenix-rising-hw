add6f500377e4577cfd00ba5f6b1bf357560f44c
Jay Glickman <jayglickman@gmail.com>
2020-01-23 15:23:36 -0500

    Part Two complete. add button works

--- w06_d3/app.js
+++ w06_d3/app.js
@@ -1,5 +1,13 @@
+const formSubmit = () => {
+    console.log($('#input-box').val())
+    $newToDo = $('#input-box').val()
+    $('#to-do-list').append($newToDo)
+    event.preventDefault();
+}
+
 $(() => {
 
+    $('#submit').on('click', formSubmit)
 
-    
+
 })
