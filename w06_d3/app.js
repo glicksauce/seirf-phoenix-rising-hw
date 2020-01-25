69f7fc8d6b367178767ac98c92aaaed34c29b746
Jay Glickman <jayglickman@gmail.com>
2020-01-24 22:57:47 -0500

    Part five complete. can remove items from list

--- w06_d3/app.js
+++ w06_d3/app.js
@@ -1,15 +1,30 @@
 const toDoItems = []
 
+const removeItem = () =>{
+    $(event.currentTarget).parent().remove();
+}
+
 const toDoUpdate = () =>{
 
 
     //change background color of item
     $(event.currentTarget).parent().css("background","#ED6495")
 
-
     //append item to completed list
     $('#completed').append($(event.currentTarget).parent())
 
+    //removes completed button listener
+    removeEventListener("click",toDoUpdate)
+
+    //updates button text
+    $(event.currentTarget).text("REMOVE")
+
+    //removes class from button, replaces with new class
+    $(event.currentTarget).removeClass("completed-Button")
+    $(event.currentTarget).addClass('remove-Button')
+
+    //adds event listener to button
+    event.currentTarget.addEventListener("click",removeItem)
 }
 
 const formSubmit = () => {
@@ -17,17 +32,24 @@ const formSubmit = () => {
 
     //new to do item uses input value and adds appropriate class
     $newToDo = $('<div>').text($('#input-box').val()).addClass("to-do-item")
+    
+    //adds item to array
     toDoItems.push($newToDo);
 
     //adds completed button to do item
     $completedButton = $('<button>').addClass("completed-Button").text("COMPLETED").css("background","#c2c3c2").css("color","#003300")
+
     $newToDo.append($completedButton)
 
     //adds event listener to completed button
     $completedButton.on('click',toDoUpdate)
 
     //appends item to to-do-list container
-    $('#to-do-list').append(toDoItems)
+    //$('#to-do-list').append(toDoItems) //if using an array use this.
+
+    $('#to-do-list').append($newToDo)
+
+    //prevent page refresh
     event.preventDefault();
 }
 
