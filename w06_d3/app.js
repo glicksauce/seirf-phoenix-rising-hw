e064795e484e0636edddaed29a51c2d463b24592
Jay Glickman <jayglickman@gmail.com>
2020-01-27 21:56:02 -0500

    more localstoreage updates

--- w06_d3/app.js
+++ w06_d3/app.js
@@ -8,39 +8,87 @@ const toDoItems = []
 
 //retrieves items from local storage
 const loadFromLocalStorage = () =>{
-    for (i = 0;i< localStorage.length;i++){
+    for (i = localStorage.length-1; i>= 0; i--){
         //console.log(localStorage[i])
         if (localStorage.key(i).slice(0,5) == "to-do" || localStorage.key(i).slice(0,5) =="compl") {
-            console.log("match")
             let item = localStorage.getItem(localStorage.key(i))
             $('#input-box').val(item)
+
             formSubmit()
+            if (localStorage.key(i).slice(0,5) =="compl") {
+                    
+                    let $currentID = $('#to-do'+(toDoNumber-1)+ ' button')
+                    modifyToDone($currentID);
+            }
+            $('#input-box').val('')
         }
-        console.log(localStorage.getItem(localStorage.key(i)))
+
         
     }
+}
+
+const modifyToDone = (divID) =>{
+    let itemID = $(divID).parent().attr("id")
+    let completedID = "completed" + completedNumber
+
+    //remove item from localStorage
+    localStorage.removeItem(itemID)
+
+    //re-add item to local storage as done
+    let textValue = $(divID).parent().text()
+    textValue = textValue.slice(0,textValue.length-9);
+    window.localStorage.setItem(completedID,textValue)
+
+    //re-labels item id
+    $(divID).parent().attr("id",completedID)
+
+    //change background color of item
+    $(divID).parent().css("background","#ED6495")
+
+    //append item to completed list
+    $('#completed').append($(divID).parent())
+
+    //removes completed button listener
+    removeEventListener("click",toDoUpdate)
+
+    //updates button text
+    $(divID).text("REMOVE")
+    
+
+    //removes class from button, replaces with new class
+    $(divID).removeClass("completed-Button")
+    $(divID).addClass('remove-Button')
+
+    //adds event listener to button
+    //console.log(divID)
+    //divID.addEventListener("click",removeItem)
+
+    //increment completed number
+    completedNumber++
 }
 
 const removeItem = () =>{
     //removes div from list
     $(event.currentTarget).parent().remove();
+    console.log($(event.currentTarget).parent().attr("id"))
+    localStorage.removeItem($(event.currentTarget).parent().attr("id"))
 }
 
 //moves from Things to Do to Things that are done
-const toDoUpdate = () =>{
+const toDoUpdate = (divText) =>{
     let itemID = $(event.currentTarget).parent().attr("id")
     let completedID = "completed" + completedNumber
 
     //remove item from localStorage
-    console.log(itemID)
     localStorage.removeItem(itemID)
 
     //re-add item to local storage as done
     let textValue = $(event.currentTarget).parent().text()
-    console.log(textValue.length);
     textValue = textValue.slice(0,textValue.length-9);
-    console.log(textValue)
     window.localStorage.setItem(completedID,textValue)
+
+    //re-labels item id
+    $(event.currentTarget).parent().attr("id",completedID)
 
     //change background color of item
     $(event.currentTarget).parent().css("background","#ED6495")
@@ -53,6 +101,7 @@ const toDoUpdate = () =>{
 
     //updates button text
     $(event.currentTarget).text("REMOVE")
+    
 
     //removes class from button, replaces with new class
     $(event.currentTarget).removeClass("completed-Button")
@@ -77,6 +126,7 @@ const formSubmit = () => {
     toDoItems.push($newToDo);
 
     //adds item to localStorage
+    //console.log(toDoNumber,itemID,$newToDo.text())
     window.localStorage.setItem(itemID,$newToDo.text())
 
     //adds completed button to do item
@@ -87,6 +137,8 @@ const formSubmit = () => {
     //adds event listener to completed button
     $completedButton.on('click',toDoUpdate)
 
+    
+
     //appends item to to-do-list container
     //$('#to-do-list').append(toDoItems) //if using an array use this.
 
@@ -96,13 +148,17 @@ const formSubmit = () => {
     toDoNumber++
 
     //prevent page refresh
-    event.preventDefault();
+    if (event != undefined){
+        event.preventDefault();
+    }
 }
 
 $(() => {
-    loadFromLocalStorage()
+    //localStorage. clear()
+
     $('#to-do-list').append(toDoItems)
     $('#submit').on('click', formSubmit)
+    loadFromLocalStorage()
 
 
 })
