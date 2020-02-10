419bd657f333bad9943d464b8ec6d2b17214ad82
Jay Glickman <jayglickman@gmail.com>
2020-02-09 20:46:15 -0500

    Tip calculator express application created

--- w09_d2/server.js
+++ w09_d2/server.js
@@ -7,6 +7,12 @@ app.get('/greeting/:name', (req, res) => {
     res.send('What\'s up ' + req.params.name + ' It\'s so great to see you!');
   });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tip = parseInt(req.params.total) * parseInt(req.params.tipPercentage)/100
+    res.send('Tip amount is ' + tip)
+});
+
+
 app.listen(port, () => {
     console.log('app is running on port: ', port)
 })
