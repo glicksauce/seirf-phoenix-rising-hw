44cfdb429e3880bab278cfd8454f08867ac6fed5
Jay Glickman <jayglickman@gmail.com>
2020-02-16 19:16:42 -0500

    Show view shows req.params.id

--- w09_d3/pokemon_app/server.js
+++ w09_d3/pokemon_app/server.js
@@ -6,6 +6,10 @@ const port    = 3000;
 const pokemon = require('./models/pokemon.js'
 )
 
+app.get('/pokemon/:id', (req,res) =>{
+    res.send(req.params.id)
+})
+
 app.get('/pokemon', (req, res) =>{
     res.render('index.ejs',
     {pokemon: pokemon})
