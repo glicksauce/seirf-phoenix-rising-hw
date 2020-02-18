e77ec748b20568c25663816ef61c1e883415eabd
Jay Glickman <jayglickman@gmail.com>
2020-02-17 19:17:04 -0500

    added table info to index.ejs

--- w10_d2/server.js
+++ w10_d2/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app     = express();
 const port    = 3000;
+app.use(express.static(__dirname + '/public'));
+
+//db file
+const Budget = require('./models/budget.js'
+)
 
 app.get('/', (req, res) =>{
-    res.send('Hello World')
+    res.render('index.ejs', {
+        Budget: Budget
+    })
 })
 
 app.listen(port, () => {
