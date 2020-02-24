2d90dcc3db9701be98e96ac1cdc6ca09c6e387dd
Jay Glickman <jayglickman@gmail.com>
2020-02-23 20:38:38 -0500

    added new.ejs that took forever

--- w10_d3/pokedex/server.js
+++ w10_d3/pokedex/server.js
@@ -8,7 +8,37 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride("_method"))
 
+//index
+app.get('/pokemon', (req, res) =>{
+    res.render('index.ejs', {Pokemon: Pokemon})
+})
 
+//new
+app.get('/pokemon/new', (req, res) =>{
+    res.render('new.ejs',
+    {
+        Pokemon: Pokemon
+    })
+})
+
+//create
+app.post('/pokemon', (req,res) => {
+    let typesArr = req.body.type.split(",")
+
+    let newPok = {
+        'name': req.body.name,
+        'img': req.body.img,
+        'id': req.body.hp,
+        'type': typesArr,
+        'stats': {'hp': req.body.hp,
+                'attack':req.body.attack,
+                'defense': req.body.defense}
+    }
+    console.log(newPok)
+
+    Pokemon.push(newPok)
+    res.redirect('/pokemon')
+})
 
 //show
 app.get('/pokemon/:id', (req, res) => {
@@ -17,10 +47,7 @@ app.get('/pokemon/:id', (req, res) => {
     })
 })
 
-//index
-app.get('/pokemon', (req, res) =>{
-    res.render('index.ejs', {Pokemon: Pokemon})
-})
+
 
 // web server
 app.listen(port, () => {
