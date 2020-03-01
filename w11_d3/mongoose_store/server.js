a6a8fe9bc45256b16fc1a4da2e0e6f18c2e361b1
Jay Glickman <jayglickman@gmail.com>
2020-03-01 13:55:42 -0500

    added seed data route

--- w11_d3/mongoose_store/server.js
+++ w11_d3/mongoose_store/server.js
@@ -14,7 +14,7 @@ Restful Routes
 
 const express = require('express')
 const app = express()
-const port = 3001;
+const port = 3002;
 const mongoose = require("mongoose")
 const methodOverride = require("method-override")
 
@@ -95,15 +95,6 @@ app.post("/products", (req,res) =>{
     res.redirect("/products")
 })
 
-// Show
-app.get("/products/:id", (req, res) => {
-  Product.findById(req.params.id, (err, showProducts) => {
-    res.render("show.ejs", {
-      products: showProducts
-    })
-  })
-})
-
 // Index
 app.get("/products", (req,res) => {
   Product.find({}, (error, products) => {
@@ -111,6 +102,50 @@ app.get("/products", (req,res) => {
   })
 })
 
+//SEED DATA ROUTE
+app.get('/products/seed', async (req, res) => {
+    const newProducts =
+      [
+        {
+          name: 'Beans',
+          description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+          img: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
+          price: 5,
+          qty: 99
+        }, {
+          name: 'Bones',
+          description: 'It\'s just a bag of bones.',
+          img: 'http://bluelips.com/prod_images_large/bones1.jpg',
+          price: 25,
+          qty: 0
+        }, {
+          name: 'Bins',
+          description: 'A stack of colorful bins for your beans and bones.',
+          img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
+          price: 7000,
+          qty: 1
+        }
+      ]
+  
+    try {
+      const seedItems = await Product.create(newProducts)
+      res.send(seedItems)
+    } catch (err) {
+      res.send(err.message)
+    }
+    res.redirect('/products')
+  })
+
+  
+// Show
+app.get("/products/:id", (req, res) => {
+    Product.findById(req.params.id, (err, showProducts) => {
+      res.render("show.ejs", {
+        products: showProducts
+      })
+    })
+  })
+  
 
 // web server
 app.listen(port, () => {
