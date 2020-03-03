fdfa4415ca942c43ee4b29b8d049ee0a8664d07a
Jay Glickman <jayglickman@gmail.com>
2020-03-03 14:11:54 -0500

    started js that formats shopping cart using jquery

--- w11_d3/mongoose_store/public/cartScript.js
+++ w11_d3/mongoose_store/public/cartScript.js
@@ -1,1 +1,34 @@
-console.log("up and running")
+$(() => {
+    //recreate shopping cart array using jquery
+    let cartLength = $('.cart').length
+    let cartArray = []
+    let finalCart = []
+    let searchIndex = -2;
+
+    for (i=0;i<cartLength;i++){
+        cartArray.push($('.cart')[i].innerHTML)
+    }
+
+    //redistribute cart by item name
+    for (j=0;j<cartArray.length;j++){
+
+        //gets index where item is found
+        searchIndex = finalCart.findIndex( ({name}) => name == cartArray[j] )
+
+        //pushes new item into array
+        if (searchIndex == -1){
+            finalCart.push(
+                {
+                    name: cartArray[j],
+                    qty: 1
+                }
+            )
+        //increases qty of found item
+        } else  {
+            finalCart[searchIndex].qty += 1;
+        }
+    }
+
+    console.log(cartArray)
+    console.log(finalCart)
+})
