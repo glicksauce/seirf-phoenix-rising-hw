c9025d86f97cdf3d8d50beff50eca6ddfc4ed738
Jay Glickman <jayglickman@gmail.com>
2020-04-16 13:00:21 -0400

    started w18_d2 homework

---
+++ w18_d2/student_solution.rb
@@ -0,0 +1,21 @@
+# https://git.generalassemb.ly/ira/SEIR-FLEX-123/tree/master/unit_4/w18d2/homework
+
+#Hello World
+#Print "Hello World" to the console
+
+p "Hello World"
+
+#After you have printed Hello World:
+
+adjective = "Big Bad"
+#Interpolate the adjective variable into the Hello World string using #{}
+#Expected output:
+#=> Hello Big Bad World
+
+p "Hello #{adjective} World"
+
+#Save "Hello World" to a variable. Without changing the letters in your code manually, permanently change "Hello World" to all uppercased letters.
+
+hello = "Hello World"
+hello.upcase!
+p hello
