73e6769949a5e174747ee2b6e34bb44fa1e8e28d
Jay Glickman <jayglickman@gmail.com>
2020-04-16 22:00:15 -0400

    euler problem 1

--- w18_d2/student_solution.rb
+++ w18_d2/student_solution.rb
@@ -164,3 +164,20 @@ p print_item_sums table, book
 p print_item_sums book, lamp
 
 puts "\n"
+
+
+###################################
+p "###################################"
+p "Euler Problem 1"
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+def findMultiples limit
+    arr = (1...limit).to_a
+    arr.select!{|num| num % 3 == 0 || num % 5 == 0}
+    p arr
+    arr.sum
+end
+
+p findMultiples 1000
