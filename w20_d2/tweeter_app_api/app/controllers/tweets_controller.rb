82d835ba5ef45b4fa8696489d9dc40d438de8ff7
Jay Glickman <jayglickman@gmail.com>
2020-04-25 16:53:57 -0400

    Commit 8: Can POST new Tweets

--- w20_d2/tweeter_app_api/app/controllers/tweets_controller.rb
+++ w20_d2/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -8,4 +8,26 @@ class TweetsController < ApplicationController
         tweet = Tweet.find(params[:id])
         render json: {status: 200, tweet: tweet}
     end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render (status 201, json: {tweet: tweet})
+        else
+            render(status: 422, json: {tweet: tweet})
+        end
+    end
+
+    def update
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render(json: {tweet: tweet})
+    end
+
+    private
+
+    def tweet_params
+        params.required(:tweet).permit(:title, :author, :content)
+    end
 end
