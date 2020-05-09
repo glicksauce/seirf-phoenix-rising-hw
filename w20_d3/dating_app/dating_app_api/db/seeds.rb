7f2d6a8668c9dafd318eb3f41b59262f4d30e724
Jay Glickman <jayglickman@gmail.com>
2020-05-09 16:08:11 -0400

    Commit 1: Made Rails Server

--- w20_d3/dating_app/dating_app_api/db/seeds.rb
+++ w20_d3/dating_app/dating_app_api/db/seeds.rb
@@ -5,3 +5,117 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+    {
+        "name": "Ryan Gosling",
+        "starsign": "Scorpio",
+        "age": 35,
+        "img": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Ryan_Gosling_in_2018.jpg",
+        "ltl": true
+    },
+    {
+        "name": " Hypatia of Alexandria  ",
+        "starsign": "Aries",
+        "age": 1700,
+        "img": "http://static1.squarespace.com/static/53e031f1e4b088c3aac80e47/t/5636ebf1e4b0683b6c56cb43/1446440030277/hypatia.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Hogan",
+        "starsign": "Libra",
+        "age": 40,
+        "img": "https://ewedit.files.wordpress.com/2019/08/tbdhohe_ec091.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Lumpy Space Princess",
+        "starsign": "Virgo",
+        "age": 29,
+        "img": "http://i0.wp.com/www.drawcentral.com/wp-content/uploads/2013/03/Lumpy-Space-Princess.jpg?resize=150%2C150",
+        "ltl": true
+    },
+    {
+        "name": "Idris Elba",
+        "starsign": "Virgo",
+        "age": 43,
+        "img": "http://www.ramascreen.com/wp-content/uploads/2015/01/Idris-Elba-Luther-150x150.jpg",
+        "ltl": true
+    },
+    {
+        "name": "The Trash Heap of Fraggle Rock",
+        "starsign": "Taurus",
+        "age": 1000,
+        "img": "http://www.toughpigs.com/wp-content/uploads/2009/12/trasheap-150x150.jpg",
+        "ltl": false
+    },
+    {
+        "name": "Guy Guy",
+        "starsign": "Gemni",
+        "age": 60,
+        "img": "https://s-media-cache-ak0.pinimg.com/236x/03/d1/53/03d153c14b49a88cab97d58a5bedb6e8.jpg",
+        "ltl": false
+    },
+    {
+        "name": "Raisin",
+        "starsign": "Gemni",
+        "age": 20,
+        "img": "https://david.brax.nu/wp-content/uploads/2010/12/Raisin.jpg",
+        "ltl": false
+    },
+    {
+        "name": "Jim",
+        "starsign": "Leo",
+        "age": 49,
+        "img": "http://www.argylejournal.com/wp-content/uploads/2012/10/Jim-Ellinghausen-e1349993531939-150x150.jpg",
+        "ltl": false
+    },
+    {
+        "name": "Muttley",
+        "starsign": "Capricorn",
+        "age": 2,
+        "img": "http://gifrific.com/wp-content/uploads/2012/08/Cartoon-Character-Mutley-Laughing-150x150.gif",
+        "ltl": true
+    },
+    {
+        "name": "Slimer",
+        "starsign": "Pisces",
+        "age": 90,
+        "img": "http://orig13.deviantart.net/ef48/f/2014/309/1/5/slimer_by_bbjeg-d85exys.png",
+        "ltl": true
+    },
+    {
+        "name": "Mist",
+        "starsign": "Aquarius",
+        "age": 340000,
+        "img": "https://worldofblackheroes.files.wordpress.com/2012/07/dr-mist.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Ching Shih",
+        "starsign": "Sagittarius",
+        "age": 340000,
+        "img": "http://treehouseletter.com/wp-content/uploads/2015/04/Ching-Shih-150x150.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Galadriel",
+        "starsign": "Sagittarius",
+        "age": 7045,
+        "img": "http://nerdbastards.com/wp-content/uploads/2015/12/Galadriel-and-Phial-150x150.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Skeletor",
+        "starsign": "Cancer",
+        "age": 19,
+        "img": "http://www.shwiggie.com/media/images/skeletor-smirk.jpg",
+        "ltl": true
+    },
+    {
+        "name": "Sauron",
+        "starsign": "Taurus",
+        "age": 198,
+        "img": "https://img1.looper.com/img/gallery/saurons-entire-backstory-explained/intro-1582030750.jpg",
+        "ltl": false
+    }
+])
